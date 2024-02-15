7f916179bd7f39a0a3f117ab0731cc06da0a2ddd
José Eduardo Alayon <jepale@hotmail.com>
2024-02-14 21:00:43 -0300

    hjg

---
+++ aleatorios.py
@@ -0,0 +1,18 @@
+import random
+
+#print("Gerar cinco números alaeatórios entre 1 e 50")
+#for i in range(5):
+#    n = random.randint(1,50)
+#    print(f"Número gerado: {n}")
+
+#valor = random.random()
+#print(f'Número gerado: {round(valor * 10, 2 )}')
+
+#valor = random.uniform(1,100)
+#print(f'Número: {round(valor, 4)}')
+
+L = [2,4,6,9,11,15,48,75,23]
+#n = random.choice(L)
+#print(f'Número escolhido: {n}')
+n = random.sample(L,3)
+print(f'Números escolhidos: {n}')
