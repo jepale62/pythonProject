2cb42768dbe2a75b540c71c04f5843b0ea838260
José Eduardo Alayon <jepale@hotmail.com>
2024-06-03 21:38:33 -0300

    hjg

--- print.py
+++ print.py
@@ -1,5 +1,22 @@
 # Sintaxe:
 # print(objetos, argumentos)
 
-mensagem = " Função Print()"
-print(mensagem)
+# mensagem = " Função Print()"
+# print(mensagem)
+
+# print('Imprime a mensagem e muda de linha')
+# print("Imprime a mensagem e permanece na linha.", end='')
+# print(' Continuo na mesma linha!')
+
+nome = 'Maria'
+idade = 30
+msg_formatada = 'O nome dela é {0} e ela tem {1} anos'.format(nome, idade)
+print(msg_formatada)
+
+msg = f'Meu nome é {nome} e tenho {idade} anos.'
+print(msg)
+
+a = 10
+b = 5
+res = f'A soma de {a} com {b} é igual a {a+b}'
+print(res)
