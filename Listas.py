6f5dadc923fc5cfbdab335211c44a822788e19a5
jepale62 <159392376+jepale62@users.noreply.github.com>
2024-07-06 20:54:37 -0300

    listas

---
+++ Listas.py
@@ -0,0 +1,13 @@
+# Lista: representa uma sequencia de valores
+# Sintaxe: nome_lista = [valores]
+
+#notas = [5,7,8,6,9]
+#print(notas)
+
+n1 = [4,6,7,8,0,3]
+n2 = [1,6,3,0,12,4]
+valores = n1 + n2
+valores[0] = 9
+print(valores)
+print(valores[0:2])
+print(valores[-2:])
