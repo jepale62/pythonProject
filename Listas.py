f5e668601cd06b8563d68d836f176cd2368c1fef
jepale62 <159392376+jepale62@users.noreply.github.com>
2024-07-13 16:42:00 -0300

    Listas

--- Listas.py
+++ Listas.py
@@ -8,6 +8,16 @@ n1 = [4,6,7,8,0,3]
 n2 = [1,6,3,0,12,4]
 valores = n1 + n2
 valores[0] = 9
-print(valores)
+'''print(valores)
 print(valores[0:2])
 print(valores[-2:])
+print(len(valores))
+print(sorted(valores, reverse=True))
+print(sum(valores))
+print(min(valores))
+print(max(valores))'''
+
+valores.append(13)
+print(valores)
+valores.pop()
+print(valores)
