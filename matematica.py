eb76a0499cf315ebd6a111f049e42d686989cc99
jepale62 <jepale@hotmail.com>
2025-10-09 16:53:07 -0300

    List_25

---
+++ matematica.py
@@ -0,0 +1,24 @@
+###valores = [2,5,8,-1,0,11,7,-6]
+#print(max(valores))
+#print(min(valores))
+
+#a = -5
+#b = 4
+#print(abs(a))
+#print(pow(a,b))
+#c = 2.789011
+#print(round(c , 3))
+
+import math
+
+x = 8
+y = 100
+
+raiz_quadrada = math.sqrt(x)
+print(raiz_quadrada)
+print(math.ceil(raiz_quadrada))
+print(math.floor( raiz_quadrada))
+print(round( raiz_quadrada, 2))
+
+logaritmo = math.log10(y)
+print(logaritmo)
--- tuplas.py
+++ tuplas.py
@@ -3,3 +3,4 @@ tupla = (2,4,6,7,9)
 print(len(tupla))
 
 #podemos concatenar as tuplas
+# operações não disponíveis em tuplas: short, append, reverse
